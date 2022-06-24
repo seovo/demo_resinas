e43b22ba575d1c4433eed1d0558baf36bd3929fd
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-23 19:25:52 -0500

    oka

--- convert_volume_purchase_js_it/models/purchase.py
+++ convert_volume_purchase_js_it/models/purchase.py
@@ -4,6 +4,6 @@ class PurchaseLine(models.Model):
     _inherit = 'purchase.order.line'
     is_volume = fields.Boolean(related="product_id.is_volume", string="Es Volumen")
     ratio_kg = fields.Float(digits=(13,10),string="Ratio KG")
-    unit_prove = fields.Float(string="Und. Prove")
+    unit_prove = fields.Many2one('uom.uom',domain=[('category_id.is_volume','=',True)],string="Und. Prove")
     cant_prov = fields.Float(string="Cant.Prov")
     prec_prov = fields.Float(string="Prec.Prov")
