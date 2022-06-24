40fc81de98d7d120456b887e202504455cce7a28
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-23 20:00:14 -0500

    oka

--- convert_volume_purchase_js_it/models/purchase.py
+++ convert_volume_purchase_js_it/models/purchase.py
@@ -8,7 +8,7 @@ class PurchaseLine(models.Model):
     cant_prov = fields.Float(string="Cant.Prov")
     prec_prov = fields.Float(string="Prec.Prov")
 
-    @api.onchange('ratio_kg','unit_prove','prec_prov')
+    @api.onchange('ratio_kg','unit_prove','prec_prov','cant_prov')
     def change_price_ps(self):
         for record in  self:
             qty = record.cant_prov * record.ratio_kg
