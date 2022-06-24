bb168a64ce9ea2196e9fa28f2e50bb3f59b2fa1a
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-23 19:47:32 -0500

    oka

--- convert_volume_purchase_js_it/models/purchase.py
+++ convert_volume_purchase_js_it/models/purchase.py
@@ -7,3 +7,11 @@ class PurchaseLine(models.Model):
     unit_prove = fields.Many2one('uom.uom',domain=[('category_id.is_volume','=',True)],string="Und. Prove")
     cant_prov = fields.Float(string="Cant.Prov")
     prec_prov = fields.Float(string="Prec.Prov")
+
+    @api.depends('ratio_kg','unit_prove','prec_prov')
+    def change_price_ps(self):
+        for record in  self:
+            record.product_qty = record.cant_prov * record.ratio_kg
+
+            tot_tmp = record.cant_prov * record.prec_prov
+            record.price_unit = tot_tmp / record.product_qty if record.product_qty != 0 else 0
--- convert_volume_purchase_js_it/models/uom.py
+++ convert_volume_purchase_js_it/models/uom.py
@@ -3,8 +3,9 @@ from odoo import fields, models , api , _
 class UomCategory(models.Model):
     _inherit = 'uom.category'
     is_volume = fields.Boolean(string="Es Volumen")
-
+'''
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
     is_volume = fields.Boolean(related="uom_id.category_id.is_volume",string="Es Volumen")
     ratio_kg = fields.Float(digits=(13,10),string="Ratio KG")
+'''
