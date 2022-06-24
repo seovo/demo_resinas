1173c70ebd7cffdb254864e119ad7bdcf247da73
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-23 19:49:42 -0500

    oka

--- convert_volume_purchase_js_it/models/purchase.py
+++ convert_volume_purchase_js_it/models/purchase.py
@@ -2,7 +2,7 @@ from odoo import fields, models , api , _
 
 class PurchaseLine(models.Model):
     _inherit = 'purchase.order.line'
-    is_volume = fields.Boolean(related="product_id.is_volume", string="Es Volumen")
+
     ratio_kg = fields.Float(digits=(13,10),string="Ratio KG")
     unit_prove = fields.Many2one('uom.uom',domain=[('category_id.is_volume','=',True)],string="Und. Prove")
     cant_prov = fields.Float(string="Cant.Prov")
