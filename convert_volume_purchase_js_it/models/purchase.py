3d205e191b2f6994460369e1a1f694c30f70d745
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-23 19:57:02 -0500

    oka

--- convert_volume_purchase_js_it/models/purchase.py
+++ convert_volume_purchase_js_it/models/purchase.py
@@ -11,7 +11,9 @@ class PurchaseLine(models.Model):
     @api.onchange('ratio_kg','unit_prove','prec_prov')
     def change_price_ps(self):
         for record in  self:
-            record.product_qty = record.cant_prov * record.ratio_kg
+            qty = record.cant_prov * record.ratio_kg
+
+            record.product_qty = qty
 
             tot_tmp = record.cant_prov * record.prec_prov
-            record.price_unit = tot_tmp / record.product_qty if record.product_qty != 0 else 0
+            record.price_unit = tot_tmp / qty if qty != 0 else 0
