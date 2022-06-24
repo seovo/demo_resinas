58ed8a8141714f8166649f3f6416c88f837e66ac
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-23 21:16:08 -0500

    oka

--- convert_volume_purchase_js_it/models/uom.py
+++ convert_volume_purchase_js_it/models/uom.py
@@ -4,13 +4,14 @@ class UomCategory(models.Model):
     _inherit = 'uom.category'
     is_volume = fields.Boolean(string="Es Volumen")
 
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-    #ratios_uom = fields.One2many('ratios.template','product_tmp_id')
-
 class RatiosRatios(models.Model):
     name = 'ratios.template'
     product_tmp_id = fields.Many2one('product.template')
     unit_prove = fields.Many2one('uom.uom', domain=[('category_id.is_volume', '=', True)],
                                  string="Und. Prove",required=True)
     ratio_kg = fields.Float(digits=(13, 10), string="Ratio KG")
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+    ratios_uom = fields.One2many('ratios.template','product_tmp_id')
+
