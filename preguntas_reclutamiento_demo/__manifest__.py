635a111258c11d861a5079a55f2348dcd24167b7
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-30 19:22:48 -0500

    agregando modulo

---
+++ preguntas_reclutamiento_demo/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Preguntas Perzonalizado',
+    'category': 'uncategorize',
+    'author': 'FULL STACK DEMO',
+    'depends': ['website_hr_recruitment'],
+    'version': '1.0',
+    'description':"""
+     Descripcion DEL MODULO
+    """,
+    'auto_install': False,
+    'demo': [],
+    'data': [
+
+        ],
+    'installable': True
+}
