d107ca7aa3cc33891a21633460da824788eb0dd8
Jesus <jesus_sanchezjibaja@hotmail.com>
2022-06-30 20:41:08 -0500

    agregando modulo

--- preguntas_reclutamiento_demo/controller/main.py
+++ preguntas_reclutamiento_demo/controller/main.py
@@ -1,8 +1,9 @@
 from odoo import http
-
+from odoo.addons.payment.models.payment_acquirer import ValidationError
 class ControlerQuestion(http.Controller):
     @http.route('/website_form_questions_hr', auth="public", methods=['POST'], website=True, csrf=False)
     def index(self,**post):
+        raise ValidationError('que pasa')
         return post
 
 
